11391b58bb3e58c7860e5586eee008f6a3f992e8
cat <cat@thenight.club>
2026-08-07 19:55:14 +0300

    Docs: A start with logging class

--- headers/Systems/Logging.hpp
+++ headers/Systems/Logging.hpp
@@ -17,15 +17,56 @@
 #include <fstream>
 
 namespace Tourmaline::Systems {
+/**
+ * @file
+ * @brief Static class used for logging
+ */
 class Logging {
 public:
+  /**
+   * @brief Log severity
+   *
+   * @note when Log::Level::Error is given as severity. The logging function
+   * will throw an std::runtime_error.
+   *
+   * @note when Log::Level::Critical is given as severity. The logging function
+   * will trigger std::terminate and shutdown the software.
+   */
   enum LogLevel { Critical, Error, Warning, Info, Debug, Trace };
 
+  /**
+   * @brief When executed, the logged messages will also be written to
+   * a file.
+   * @param File optional path for the log file. If not specified the file name
+   * will be `./Tourmaline-yy-mm-dd.txt`
+   */
   static void LogToFile(Corrade::Containers::String File = "");
+
+  /**
+   * @brief Non-formating logging function.
+   *
+   * @param message The body of the message.
+   * @param position Where this log originates from. Specifying this while
+   * optional is heavily suggested.
+   * @param severity How severe is this log message, see @ref
+   * Tourmaline::Systems::Logging::LogLevel .
+   * @param assertion when set to `true` the log message will actually be sent,
+   * otherwise it will be ignored.
+   */
   static void Log(Corrade::Containers::StringView message,
                   Corrade::Containers::StringView position = "Unknown",
                   LogLevel severity = LogLevel::Info, bool assertion = true);
 
+  /**
+   * @brief Formating logging function.
+   *
+   * @param format The body of the message, when `{}` is found it will be used
+   * to substitute with an argument.
+   * @param position Where this log originates from.
+   * @param severity How severe is this log message, see @ref
+   * Tourmaline::Systems::Logging::LogLevel .
+   * @tparam Args args arguments to format the log with.
+   */
   template <class... Args>
   static void LogFormatted(const char *format, const char *position,
                            LogLevel severity, const Args &...args) {
