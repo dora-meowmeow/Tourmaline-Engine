2ff430f2ed7fe211e5f293d7b05efd165c782dac
cat <cat@thenight.club>
2026-08-10 01:54:01 +0300

    Docs: Added docs to Random.hpp

--- headers/Systems/Random.hpp
+++ headers/Systems/Random.hpp
@@ -13,15 +13,34 @@
 #include "TourmalineExternal/random/xoshiro.h"
 
 #include <type_traits>
-
+/**
+ * @file
+ * @brief Built-in random generation
+ */
 namespace Tourmaline::Systems {
+/**
+ * @brief Static random generation class. The random generation uses xoshiro.h.
+ * @see https://github.com/david-cortes/xoshiro_cpp/blob/master/xoshiro.h
+ */
 class Random {
 public:
+  /**
+   * @brief Generates a random value.
+   * @tparam T any type that satisfies std::is_integral_v
+   * @param max maximum inclusive value to generate.
+   * @param min minimum inclusive value to generate.
+   * @return a random value same type as max and min.
+   */
   template <typename T>
     requires std::is_integral_v<T>
   static T Generate(T max, T min = 0) {
     return (generator() % (max - min + 1)) + min;
   }
+
+  /**
+   * @brief Generates a random Tourmaline::Type::UUID.
+   * @return a random Tourmaline::Type::UUID.
+   */
   static Tourmaline::Type::UUID GenerateUUID();
 
 private:
