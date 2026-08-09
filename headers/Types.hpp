ac932f3fe0c3650238fa24cb56f007ce28f0ebdb
cat <cat@thenight.club>
2026-08-10 01:39:08 +0300

    Docs: Documenting Types.hpp

--- headers/Types.hpp
+++ headers/Types.hpp
@@ -16,24 +16,52 @@
 #include <cstdint>
 #include <functional>
 #include <string>
-
+/**
+ * @file
+ * @brief Custom types used by Tourmaline.
+ */
 namespace Tourmaline::Type {
+/**
+ * @brief A 128-bit UUID with hashing support.
+ * @see https://www.rfc-editor.org/rfc/rfc9562.html#name-uuid-version-4
+ */
 class UUID {
 public:
   [[nodiscard]]
+  /**
+   * @brief Outputs the UUID as a string.
+   */
   Corrade::Containers::String asString() const;
   bool operator==(const UUID &rhs) const;
 
+  /**
+   * @brief Creates an UUID with 2 specified 64-bit unsigned integers.
+   * @see https://www.rfc-editor.org/rfc/rfc9562.html#name-uuid-version-4
+   * @param firstHalf a 64-bit unsigned integer. This is 0 to 63rd bit of the
+   * UUID.
+   * @param secondHalf a 64-bit unsigned integer. This is 64th to 127th bit of
+   * the UUID.
+   */
   UUID(uint64_t firstHalf = 0, uint64_t secondHalf = 0);
+  /**
+   * @brief Create an UUID with a string.
+   * @param uuid Must be a hex representation with no dividers.
+   * Example: "6B62E1F98D234BE92E6D93A3C7B0FDCA" is a valid UUID
+   * @warning This function does not check if the given UUID string is valid!
+   */
   UUID(const std::string &uuid);
 
   uint64_t firstHalf, secondHalf;
 };
 
-// standard library might have something that does this
-struct UnspecifiedType;
+/**
+ * @brief A placeholder struct for templates. If a template has a type
+ * set as this, that type (and possibly the argument itself) is optional.
+ */
+struct UnspecifiedType {};
 } // namespace Tourmaline::Type
 
+/// @cond
 namespace std {
 template <> struct hash<Tourmaline::Type::UUID> {
   size_t operator()(const Tourmaline::Type::UUID &uuid) const noexcept {
@@ -44,5 +72,5 @@ template <> struct hash<Tourmaline::Type::UUID> {
 };
 
 } // namespace std
-
+/// @endcond
 #endif
