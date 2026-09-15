c5040f24981ccf38bae7347baf7d7c448c6457e9
cat <cat@thenight.club>
2026-09-15 20:56:40 +0300

    Docs: Documenting Importer.hpp

---
+++ doxygen/documents/supportedFiles.md
@@ -0,0 +1,28 @@
+\page supportedFileTypes Supported File-types
+
+# How does Tourmaline import assets?
+
+Tourmaline Engine heavily relies on [Magnum Graphics' importer plugins](https://github.com/mosra/magnum-plugins) under the hood. This is by design, as Magnum Graphics is the graphics back end for Tourmaline Engine. It is heavily suggested that you [get yourself familiarised on how Magnum Graphics and it's importers work!](https://doc.magnum.graphics/magnum/) You can check out Tourmaline::Systems::Importer to learn more about how to import assets.
+
+Currently the following importers are compiled when Tourmaline Engine compiles Magnum Graphics as a dependency (This list is not final and may grow/shrink/change overtime):
+
+## Images/Textures
+
+- [StbiImporter](https://doc.magnum.graphics/magnum/classMagnum_1_1Trade_1_1StbImageImporter.html) adds capabilities of [stb_image](https://github.com/nothings/stb). Supports:
+  - `*.bmp`
+  - `*.gif`
+  - `*.hdr`
+  - `*.jpg/jpe/jpeg`
+  - `*.pgm`
+  - `*.pic`
+  - `*.png`
+  - `*.ppm`
+  - `*.psd`
+  - `*.tga/vda/icb/vst`
+
+## 3D
+
+- [ObjImporter](https://doc.magnum.graphics/magnum/classMagnum_1_1Trade_1_1ObjImporter.html) supports:
+  - `*.obj`
+
+@note Tourmaline Engine also compiles AnyImageImporter and AnySceneImporter for ease of use.
--- headers/Systems/Importer.hpp
+++ headers/Systems/Importer.hpp
@@ -16,26 +16,110 @@
 #include "Magnum/GL/GL.h"
 #include "Magnum/GL/Mesh.h"
 #include "Magnum/GL/Sampler.h"
+#include "Magnum/GL/Texture.h"
 #include "Magnum/Trade/AbstractImporter.h"
 #include "Magnum/Trade/ImageData.h"
 
-namespace Tourmaline::Systems {
+/**
+ * @file
+ * @brief Asset importing related features
+ */
 
+namespace Tourmaline::Systems {
+/**
+ * @brief A static class that simplifies importing assets.
+ *
+ * Assets used in Tourmaline Engine are stored in Magnum Graphics' own classes
+ * (Specifically Magnum::GL namespace). This is due to the backend rendering
+ * being mostly done by Magnum Graphics.
+ *
+ * It is heavily suggested that you [get yourself familiarised with how Magnum
+ * Graphics works.](https://doc.magnum.graphics/magnum/)
+ *
+ * @see [Magnum Graphics' Magnum::GL namespace
+ * documentation](https://doc.magnum.graphics/magnum/namespaceMagnum_1_1GL.html)
+ */
 class Importer {
 public:
+  /**
+   * @brief Imports an image asset as a ImageData2D.
+   *
+   * @param path Path to the asset. See @ref supportedFileTypes to learn
+   * which file-types are supported.
+   *
+   * @return The image asset imported as an ImageData2D.
+   *
+   * @warning If this function fails to find the specified asset at given path,
+   * it will throw!
+   *
+   * @see [Magnum::Trade::ImageData](
+   * https://doc.magnum.graphics/magnum/classMagnum_1_1Trade_1_1ImageData.html)
+   */
   static Magnum::Trade::ImageData2D
   LoadImage2D(Corrade::Containers::StringView path);
+
+  /**
+   * @brief Converts an ImageData2D to Texture2D.
+   *
+   * @param image The image data to use.
+   * @param filterType The type of texture filtering to prefer.
+   * @param wrappingRule The way the image should be wrapped.
+   *
+   * @return The image data as a Texture2D.
+   *
+   * @see [Magnum::Trade::ImageData](
+   * https://doc.magnum.graphics/magnum/classMagnum_1_1Trade_1_1ImageData.html)
+   * @see [Magnum::GL::SamplerFilter](
+   * https://doc.magnum.graphics/magnum/namespaceMagnum_1_1GL.html#ac5625b1b6d9959c520df40ccaca38f70)
+   * @see [Magnum::GL::SamplerWrapping](
+   * https://doc.magnum.graphics/magnum/namespaceMagnum_1_1GL.html#ae807f5b816812ec007253cae4ddc4520)
+   */
   static Magnum::GL::Texture2D LoadTexture2D(
       Magnum::Trade::ImageData2D image,
       Magnum::GL::SamplerFilter filterType = Magnum::GL::SamplerFilter::Nearest,
       Magnum::GL::SamplerWrapping wrappingRule =
           Magnum::GL::SamplerWrapping::ClampToEdge);
 
+  /**
+   * @brief Imports an image asset as a Texture2D.
+   *
+   * @param path Path to the asset. See @ref supportedFileTypes to learn
+   * which file-types are supported.
+   * @param filterType The type of texture filtering to prefer.
+   * @param wrappingRule The way the image should be wrapped.
+   *
+   * @return The image imported as a Texture2D.
+   *
+   * @warning If this function fails to find the specified asset at given path,
+   * it will throw!
+   *
+   * @see [Magnum::GL::Texture](
+   * https://doc.magnum.graphics/magnum/classMagnum_1_1GL_1_1Texture.html)
+   * @see [Magnum::GL::SamplerFilter](
+   * https://doc.magnum.graphics/magnum/namespaceMagnum_1_1GL.html#ac5625b1b6d9959c520df40ccaca38f70)
+   * @see [Magnum::GL::SamplerWrapping](
+   * https://doc.magnum.graphics/magnum/namespaceMagnum_1_1GL.html#ae807f5b816812ec007253cae4ddc4520)
+   */
   static Magnum::GL::Texture2D LoadTexture2D(
       Corrade::Containers::StringView path,
       Magnum::GL::SamplerFilter filterType = Magnum::GL::SamplerFilter::Nearest,
       Magnum::GL::SamplerWrapping wrappingRule =
           Magnum::GL::SamplerWrapping::ClampToEdge);
+
+  /**
+   * @brief Imports an 3D asset as a GL::Mesh.
+   *
+   * @param path Path to the asset. See @ref supportedFileTypes to learn
+   * which file-types are supported.
+   *
+   * @return The 3D asset as a GL::Mesh.
+   *
+   * @warning If this function fails to find the specified asset at given path,
+   * it will throw!
+   *
+   * @see [Magnum::GL::Mesh](
+   * https://doc.magnum.graphics/magnum/classMagnum_1_1GL_1_1Mesh.html)
+   */
   static Magnum::GL::Mesh LoadObject(Corrade::Containers::StringView path);
 
 private:
