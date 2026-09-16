11a064b65c4335b6d25b7ff0e93a18f5e52f2267
cat <cat@thenight.club>
2026-09-16 22:33:23 +0300

    Axis: Added KeyAxis2D.

--- headers/Game/Input/Axis.hpp
+++ headers/Game/Input/Axis.hpp
@@ -15,6 +15,8 @@
  * @brief Input axising
  */
 #include "Keyboard.hpp"
+#include "Magnum/Math/Vector2.h"
+
 #include <type_traits>
 
 namespace Tourmaline::Game::Input {
@@ -38,13 +40,48 @@ template <typename returnType>
   requires(std::is_integral_v<returnType> ||
            std::is_floating_point_v<returnType>) &&
           std::is_signed_v<returnType>
-returnType KeyAxis1D(const Input::Key &positiveKey,
-                     const Input::Key &negativeKey,
-                     Input::KeyState stateToCompare = Input::KeyState::Held,
-                     returnType scale = 1) {
+returnType KeyAxis(const Input::Key &positiveKey, const Input::Key &negativeKey,
+                   Input::KeyState stateToCompare = Input::KeyState::Held,
+                   returnType scale = 1) {
   return static_cast<returnType>((positiveKey.state == stateToCompare) -
                                  (negativeKey.state == stateToCompare)) *
          scale;
+}
+
+/**
+ * @brief Vector2D variant of Tourmaline::Game::Input::KeyAxis.
+ *
+ * @tparam returnType The type to return as. Must be signed and either an
+ * integer or a float type.
+ *
+ * @param xPositiveKey Key to be the positive value on the X axis.
+ * @param xNegativeKey Key to be the negative value on the X axis.
+ * @param yPositiveKey Key to be the positive value on the Y axis.
+ * @param yNegativeKey Key to be the negative value on the Y axis.
+ * @param stateToCompare Which state should the keys be compared. Default is
+ * KeyState::Held.
+ * @param scale Scales the result to specificed value.
+ *
+ * @return Vector2D axis multiplied scale.
+ * __**If returnType is a floating point**__, vector2D is normalised then
+ * multiplied with scale.
+ */
+template <typename returnType>
+  requires(std::is_integral_v<returnType> ||
+           std::is_floating_point_v<returnType>) &&
+          std::is_signed_v<returnType>
+Magnum::Math::Vector2<returnType>
+KeyAxis2D(const Input::Key &xPositiveKey, const Input::Key &xNegativeKey,
+          const Input::Key &yPositiveKey, const Input::Key &yNegativeKey,
+          Input::KeyState stateToCompare = Input::KeyState::Held,
+          returnType scale = 1) {
+  Magnum::Math::Vector2<returnType> result{
+      KeyAxis(xPositiveKey, xNegativeKey, stateToCompare, 1),
+      KeyAxis(yPositiveKey, yNegativeKey, stateToCompare, 1)};
+  if constexpr (std::is_floating_point_v<returnType>) {
+    return result.length() ? result.normalized() * scale : result;
+  }
+  return result * scale;
 }
 
 } // namespace Tourmaline::Game::Input
