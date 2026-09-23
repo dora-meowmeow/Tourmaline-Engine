15111612e8a3a3750b053e5eecda68d2e2905cc7
cat <cat@thenight.club>
2026-09-23 06:30:04 +0300

    Docs: Added Serialization.hpp and DataHandling.hpp

--- headers/Systems/Serialization.hpp
+++ headers/Systems/Serialization.hpp
@@ -11,23 +11,86 @@
 #define GUARD_TOURMALINE_SERIALIZATION_H
 #include <bit>
 #include <cstdint>
+#include <memory>
 #include <string>
+#include <string_view>
 #include <tuple>
 #include <utility>
 
 #include "Serialization/DataHandling.hpp"
 
-namespace Tourmaline::Systems::Serialization {
-template <typename Type> struct Serializable {
-  virtual ~Serializable() = default;
+/**
+ * @file
+ * @brief Built-in serialization feature
+ */
 
+namespace Tourmaline::Systems::Serialization {
+/**
+ * @brief When inherited publically, it allows serialization of a class.
+ *
+ * @tparam Type Type of the class that is inheriting from (CRTP).
+ *
+ * Marks a class/struct as serializable. This allows Tourmaline to serialize,
+ * and deserialize any member with given pointer inside `serialList` (see note).
+ *
+ * @note Every class that inherits Serializable must declare a
+ * `static inline SerialList serialList{};`. The name MUST be `serialList`.
+ * see @ref Tourmaline::Systems::Serialization::Serializable::SerialList to
+ * learn more.
+ *
+ * @warning As stated in the note before it must be static, also the type and
+ * the name must be EXACT. You cannot name it anything else or use a different
+ * type or make it not static. It may not be inline, if you prefer.
+ */
+template <typename Type> struct Serializable {
+  /**
+   * @brief Member pointer list(tuple) of every member that should be
+   * serialized.
+   *
+   * @tparam dataType The data type of each member pointer. Should resolve
+   * automatically.
+   *
+   * This struct will hold every member point that is requested to be
+   * serialized/deserialized. This does allow you to skip some members of a
+   * class from serialization/deserialization. Any member that is not
+   * serialized/deserialized, will not be affected/changed.
+   *
+   * @note As stated in @ref Tourmaline::Systems::Serialization::Serializable
+   * Each class/struct that inherits Serializable requires to define `static
+   * inline SerialList serialList{};`. Everything except the `inline` is
+   * required. The name MUST be `serialList` and nothing else.
+   */
   template <typename... dataType>
   struct SerialList : public std::tuple<dataType Type::*...> {
+    /**
+     * @brief Constructs the list with given member pointers.
+     *
+     * @param memberPointers Every member that should be serialized's member
+     * pointer (example: &x::y).
+     *
+     * Under the hood this just generates an std::tuple. However it deals with a
+     * lot of type specification requirements.
+     */
     SerialList(dataType Type::*...memberPointers)
         : std::tuple<dataType Type::*...>(memberPointers...) {}
+    /**
+     * @brief Amount of members to serialize/deserialize.
+     */
     static constexpr std::size_t Count = sizeof...(dataType);
   };
 
+  virtual ~Serializable() = default;
+
+  /**
+   * @brief Serializes the class/struct members in serialList into a string.
+   *
+   * For each type a ToData and FromData must be defined for proper
+   * serialization/deserialization. See
+   * @ref Tourmaline::Systems::Serialization::ToData and
+   * @ref Tourmaline::Systems::Serialization::FromData.
+   *
+   * @return The serialized data of the class.
+   */
   std::string Serialize() {
     const Type &instance = getTypeInstance();
     auto serialList = getSerialList();
@@ -37,19 +100,36 @@ template <typename Type> struct Serializable {
     }(std::make_integer_sequence<std::size_t, serialList.Count>{});
   }
 
-  void Deserialize(std::string serialData) {
+  /**
+   * @brief Deserializes the string into the class/struct's members in
+   * serialList.
+   *
+   * @param serialData The raw serialized data from Serialize() function.
+   *
+   * For each type a ToData and FromData must be defined for proper
+   * serialization/deserialization. See
+   * @ref Tourmaline::Systems::Serialization::ToData and
+   * @ref Tourmaline::Systems::Serialization::FromData.
+   */
+  void Deserialize(std::string_view serialData) {
     Type &instance = getTypeInstance();
     auto serialList = getSerialList();
-    char *data = serialData.data();
-    uint8_t serialEndianess = data[0];
+    const char *data = serialData.data();
+    uint8_t serialEndianess = serialData[0];
 
     if (serialEndianess == (std::endian::native == std::endian::little))
         [[likely]] {
       const uint32_t count = *reinterpret_cast<const uint32_t *>(data + 1);
-      char *dataPointers[count]; // This is char* since data length is unit byte
+
+      // Doing this to not rely on VLA
+      auto dataStore = std::make_unique<const char *[]>(count);
+
+      // This is a char since data length is unit byte
+      const char **dataPointers = dataStore.get();
       dataPointers[0] = data + sizeof(uint32_t) + 1;
       for (uint32_t x = 1; x < count; x++) {
-        uint32_t length = *reinterpret_cast<uint32_t *>(dataPointers[x - 1]);
+        const uint32_t length =
+            *reinterpret_cast<const uint32_t *>(dataPointers[x - 1]);
         dataPointers[x] = dataPointers[x - 1] + sizeof(uint32_t) + length;
       }
       [&]<std::size_t... index>(std::index_sequence<index...>) {
@@ -60,6 +140,11 @@ template <typename Type> struct Serializable {
     }
   }
 
+  /**
+   * @brief Returns the class instance that inherits this class (CRTP).
+   *
+   * @return The class instance as a reference.
+   */
   Type &getTypeInstance() const {
     return *const_cast<Type *>(reinterpret_cast<const Type *>(this));
   };
--- headers/Systems/Serialization/DataHandling.hpp
+++ headers/Systems/Serialization/DataHandling.hpp
@@ -11,25 +11,65 @@
 #define GUARD_TOURMALINE_SERIAL_DATAHANDLING_H
 #include <cstdint>
 #include <string>
+#include <tuple>
 #include <type_traits>
 
+/**
+ * @file
+ * @brief How-to serialize data
+ */
 namespace Tourmaline::Systems::Serialization {
 template <typename Type> struct Serializable;
 
 // Default
+/**
+ * @brief Template function used to convert a data type to a string for
+ * serialization.
+ *
+ * @tparam type The type this function applies to. You can also use C++20
+ * concepts to make a more broader function.
+ *
+ * @return The data as a string for serialization.
+ *
+ * @warning The first 4 bytes must contain the length of the data being written.
+ * So if you are writing 20 bytes of data first 4 bytes in uint32_t must be 20.
+ * Failure to add the proper length will break rest of the serialization.
+ */
 template <typename type> static std::string ToData(type) {
   static_assert(false, "ToData function requires a overload defined for this "
                        "type to serialize data.");
   throw;
 }
 
-template <typename type> static void FromData(char *, type &) {
+/**
+ * @brief Template function used to convert a  to a string for
+ * serialization.
+ *
+ * @tparam type The type this function applies to. You can also use C++20
+ * concepts to make a more broader function.
+ *
+ * @param dataStart Where the data starts. First 4 bytes will contain the
+ * length of the data.
+ * @param result Reference to where to write the result of deserialization.
+ *
+ * @warning First 4 bytes in uint32_t will contain how long the data inside
+ * dataStart is. So if data stored in dataStart is 20 bytes, first 4 bytes
+ * in uint32_t will be 20.
+ */
+template <typename type>
+static void FromData(const char *dataStart, type &result) {
+  // To supress warns
+  std::ignore = dataStart;
+  std::ignore = result;
   static_assert(false, "FromData function requires a overload defined for this "
                        "type to deserialize data.");
   throw;
 }
 
 // Functions
+/**
+ * @brief Functions cannot be serialized/deserialized.
+ */
 template <typename type>
   requires std::is_function_v<type>
 static std::string ToData(type) {
@@ -37,14 +77,24 @@ static std::string ToData(type) {
   throw;
 }
 
+/**
+ * @brief Functions cannot be serialized/deserialized.
+ */
 template <typename type>
   requires std::is_function_v<type>
-static void FromData(char *, type &) {
+static void FromData(const char *, type &) {
   static_assert(false, "Functions cannot be deserialized!");
   throw;
 }
 
 // Pointers
+/**
+ * @brief Currently pointers and references are not supported for
+ * serialization/deserialization.
+ *
+ * It is still possible to do so, if per type pointer/reference is defined for
+ * ToData/FromData.
+ */
 template <typename type>
   requires std::is_reference_v<type> || std::is_pointer_v<type>
 static std::string ToData(type) {
@@ -55,7 +105,26 @@ static std::string ToData(type) {
   throw;
 }
 
+/**
+ * @brief Currently pointers and references are not supported for
+ * serialization/deserialization.
+ *
+ * It is still possible to do so, if per type pointer/reference is defined for
+ * ToData/FromData.
+ */
+template <typename type>
+  requires std::is_reference_v<type> || std::is_pointer_v<type>
+static void FromData(const char *, type &) {
+  static_assert(false, "Functions cannot be deserialized!");
+  throw;
+}
+
 // Classes with Serialization support
+/**
+ * @brief Calls Serialize() recursively so class inside a class can be
+ * serialized as well. Class inside the class also needs to inherits
+ * Serializable.
+ */
 template <typename type>
   requires std::is_base_of_v<Serializable<type>, type>
 static std::string ToData(type value) {
@@ -67,15 +136,23 @@ static std::string ToData(type value) {
   return result;
 }
 
+/**
+ * @brief Calls Deserialize() recursively so class inside a class can be
+ * deserialized as well. Class inside the class also needs to inherits
+ * Serializable.
+ */
 template <typename type>
   requires std::is_base_of_v<Serializable<type>, type>
-static void FromData(char *entryLocation, type &result) {
-  uint32_t length = *reinterpret_cast<uint32_t *>(entryLocation);
+static void FromData(const char *entryLocation, type &result) {
+  const uint32_t length = *reinterpret_cast<const uint32_t *>(entryLocation);
   std::string data(entryLocation + sizeof(uint32_t), length);
   result.Deserialize(data);
 }
 
 // Fundamental data types
+/**
+ * @brief Fundamental types will be serialized automatically.
+ */
 template <typename type>
   requires std::is_fundamental_v<type>
 static std::string ToData(type value) {
@@ -87,11 +164,15 @@ static std::string ToData(type value) {
   return result;
 }
 
+/**
+ * @brief Fundamental types will be deserialized automatically.
+ */
 template <typename type>
   requires std::is_fundamental_v<type>
-static void FromData(char *entryLocation, type &result) {
+static void FromData(const char *entryLocation, type &result) {
   // Length is not necessary with fundamentals
-  result = *reinterpret_cast<type *>(entryLocation + 4);
+  result =
+      *const_cast<type *>(reinterpret_cast<const type *>(entryLocation + 4));
 }
 } // namespace Tourmaline::Systems::Serialization
 #endif
